d856ba6185ceaf54641c236435e9113c70d068ef
Alex Robson <asrobson@gmail.com>
2011-03-25 00:48:33 -0700

    Trying to add management plugins to the Rabbit install script.

--- install-couchbase.sh
+++ install-couchbase.sh
@@ -4,4 +4,5 @@ cd src
 mkdir couchbase
 cd couchbase
 wget http://c3145442.r42.cf0.rackcdn.com/couchbase-server-community_x86_1.1.deb
+sudo apt-get -y remove couchdb-bin
 sudo dpkg -i couchbase-server-community_x86_1.1.deb
--- install-rabbitmq-plugins.sh
+++ install-rabbitmq-plugins.sh
@@ -1,17 +1,4 @@
-cd /
-mkdir src
-cd src
-mkdir rabbitmq
-cd rabbitmq
-
-sudo apt-get install erlang-nox
-sudo apt-get -fy install
-
-wget http://www.rabbitmq.com/releases/rabbitmq-server/v2.4.0/rabbitmq-server_2.4.0-1_all.deb
-
-sudo dpkg -i rabbitmq-server_2.4.0-1_all.deb
-sudo /etc/init.d/rabbitmq-install stop
-
+sudo /etc/init.d/rabbitmq-server stop
 cd /usr/lib/rabbitmq/plugins
 
 sudo wget http://www.rabbitmq.com/releases/plugins/v2.4.0/mochiweb-2.4.0.ez
@@ -21,4 +8,4 @@ sudo wget http://www.rabbitmq.com/releases/plugins/v2.4.0/rabbitmq-mochiweb-2.4.
 sudo wget http://www.rabbitmq.com/releases/plugins/v2.4.0/rabbitmq-management-agent-2.4.0.ez
 sudo wget http://www.rabbitmq.com/releases/plugins/v2.4.0/rabbitmq-management-2.4.0.ez
 
-sudo /etc/init.d/rabbitmq-install start
+sudo /etc/init.d/rabbitmq-server start 
