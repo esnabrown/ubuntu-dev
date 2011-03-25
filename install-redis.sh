82a992edc712deb60bcca8aaf7006ccb73236344
Alex Robson <asrobson@gmail.com>
2011-03-24 23:46:19 -0700

    Adding Redis to the mix

---
+++ install-redis.sh
@@ -0,0 +1,14 @@
+cd /
+mkdir src
+cd src
+mkdir redis
+cd redis
+wget http://redis.googlecode.com/files/redis-2.2.2.tar.gz
+tar xzf redis-2.2.2.tar.gz
+cd redis-2.2.2
+sudo make
+cd src
+sudo mkdir /usr/lib/redis
+sudo cp * /usr/lib/redis/
+sudo ln -s /usr/lib/redis/redis-server /usr/bin/redis-server
+sudo chmod 777 /usr/bin/redis-server
