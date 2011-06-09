e467c3a67712629f64fb6bb2cfc3e8095f2cd005
Alex Robson <alex@sharplearningcurve.com>
2011-06-09 10:51:37 -0700

    Correcting for missing dependencies in erlang build that caused exclusion of erlang crypto and broke rebar.

--- install-erlang14b03.sh
+++ install-erlang14b03.sh
@@ -1,5 +1,5 @@
 # You will need to make this file executable (chmod u+x) and run it with sudo
-apt-get -y install build-essential m4 libncurses5-dev libssh-dev unixodbc-dev libgmp3-dev libwxgtk2.8-dev libglu1-mesa-dev fop xsltproc default-jdk
+apt-get -y install build-essential m4 libncurses5-dev openssl lobssl-dev libssh-dev unixodbc-dev libgmp3-dev libwxgtk2.8-dev libglu1-mesa-dev fop xsltproc default-jdk
 mkdir -p /src/erlang
 cd /src/erlang
 wget http://www.erlang.org/download/otp_src_R14B03.tar.gz
