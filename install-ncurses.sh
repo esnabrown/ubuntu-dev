997eb01b7313a0685079e34f4156a5996bff63bf
Alex Robson <alex@sharplearningcurve.com>
2011-06-10 08:22:18 -0700

    Fixing the ncurses install script to ignore the ssl cert on launchpad

--- install-ncurses.sh
+++ install-ncurses.sh
@@ -1,7 +1,7 @@
 cd /src
 mkdir ncurses
 cd ncurses
-sudo wget https://launchpad.net/ubuntu/+archive/primary/+files/ncurses_5.7%2B20100626.orig.tar.gz
+sudo wget --no-check-certificate https://launchpad.net/ubuntu/+archive/primary/+files/ncurses_5.7%2B20100626.orig.tar.gz
 sudo chmod 666 ncurses_5.7+20100626.orig.tar.gz
 tar -xzvf ncurses_5.7+20100626.orig.tar.gz
 cd ncurses-5.7-20100626
